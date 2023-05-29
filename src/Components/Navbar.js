dfa857beacfb26e520091e6e1d124d3cafe06684
EziKielCroat <alahairlines@gmail.com>
2023-05-30 00:05:57 +0200

    Lightmode navbar napravljen

---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+
+import './Navbar.css'
+import { Search } from '@mui/icons-material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+function Navbar() {
+
+    return(
+        <nav className="Navbar">
+        <div className="search-container">
+          <Search className="search-icon search-icon-change" />
+          <input placeholder="Search for accounts, transactions..." type="search" className="search-input"/>
+        </div>
+        <div className="move-button">
+          <button onClick={() =>{console.log("move page.")}}>Move</button>
+        </div>
+        <div className="action-buttons">
+          <button className="create-account-button">Create account</button>
+          <button className="expand-more-button"><ExpandMoreIcon className="expand-more-icon expand-more-icon-change" /></button>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar
