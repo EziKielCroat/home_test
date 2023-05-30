20f6ae6a6e0a90850b9a737c685333a388b0122d
EziKielCroat <alahairlines@gmail.com>
2023-05-30 23:51:49 +0200

    dodano misto za transakcije, mobile napravit + settings modal

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,21 +1,44 @@
-
 import './Main.css';
-
+import { useRef } from 'react';
 function Main() {
 
-    return(
-        <main className="Main">
-            <h2>See what's going on Polkadot</h2>
+    const mainRef = useRef(null);
 
-            {/*ovaj prozor samo pokazat ako korisnik nije ulogiran */}
-            <div className="get-started">
-                <h3>Get started with Polkadot!</h3>
+    const handleMouseEnter = () => {
+      mainRef.current.classList.add('hovered');
+    };
+  
+    const handleMouseLeave = () => {
+      mainRef.current.classList.remove('hovered');
+    };
 
-                <p>Explore the unique opportunities of the digital currency Polkadot</p>
-                <button onClick={() =>{console.log("create-account.")}} className="create-account-button">Create account</button>
-            </div>
-        </main>
-    )
+  return (
+    <main className="Main" ref={mainRef} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <h2>See what's going on Polkadot</h2>
+
+      {/*ovaj prozor samo pokazat ako korisnik nije ulogiran */}
+      <div className="get-started">
+        <h3>Get started with Polkadot!</h3>
+
+        <p>Explore the unique opportunities of the digital currency Polkadot</p>
+        <button onClick={() => { console.log("create-account.") }} className="create-account-button">Create account</button>
+      </div> 
+
+      <h2>See transactions on Polkadot chain.</h2>
+      <p>TBD.</p>
+      <p>ako neskuzin kako prikazat sve transakcije(da dobro izgleda), mogu probat prikazat samo transakcije korisnika prijavljene na platformu.</p>
+      <p>Qui qui ea facilis quia. In corrupti quia et nihil. Itaque velit a est. Nesciunt asperiores culpa dolores saepe accusantium et laboriosam minima.</p>
+      <p>Qui qui ea facilis quia. In corrupti quia et nihil. Itaque velit a est. Nesciunt asperiores culpa dolores saepe accusantium et laboriosam minima.</p>
+      <p>Qui qui ea facilis quia. In corrupti quia et nihil. Itaque velit a est. Nesciunt asperiores culpa dolores saepe accusantium et laboriosam minima.</p>
+      <p>Qui qui ea facilis quia. In corrupti quia et nihil. Itaque velit a est. Nesciunt asperiores culpa dolores saepe accusantium et laboriosam minima.</p>
+      <p>Qui qui ea facilis quia. In corrupti quia et nihil. Itaque velit a est. Nesciunt asperiores culpa dolores saepe accusantium et laboriosam minima.</p>
+      <p>Qui qui ea facilis quia. In corrupti quia et nihil. Itaque velit a est. Nesciunt asperiores culpa dolores saepe accusantium et laboriosam minima.</p>
+      <p>Qui qui ea facilis quia. In corrupti quia et nihil. Itaque velit a est. Nesciunt asperiores culpa dolores saepe accusantium et laboriosam minima.</p>
+      <p>Qui qui ea facilis quia. In corrupti quia et nihil. Itaque velit a est. Nesciunt asperiores culpa dolores saepe accusantium et laboriosam minima.</p>
+      <p>Qui qui ea facilis quia. In corrupti quia et nihil. Itaque velit a est. Nesciunt asperiores culpa dolores saepe accusantium et laboriosam minima.</p>
+      <p>Qui qui ea facilis quia. In corrupti quia et nihil. Itaque velit a est. Nesciunt asperiores culpa dolores saepe accusantium et laboriosam minima.</p>
+    </main>
+  );
 }
 
-export default Main
+export default Main;
