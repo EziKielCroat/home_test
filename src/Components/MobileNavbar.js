72fd109f0431b040f5941e234ef167c2f5c71de2
EziKielCroat <alahairlines@gmail.com>
2023-06-08 17:54:49 +0200

    gotov sa mobilenavbar triba jos search napravit za mob i pc

--- src/Components/MobileNavbar.js
+++ src/Components/MobileNavbar.js
@@ -1,5 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './MobileNavbar.css';
+
+import HouseIcon from '@mui/icons-material/House';
+import PersonIcon from '@mui/icons-material/Person';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 
 function MobileNavbar({ visible, setVisibleOriginal }) {
   const mobileNavbarRef = useRef(null);
@@ -35,12 +40,31 @@ function MobileNavbar({ visible, setVisibleOriginal }) {
     }
   };
 
+  const handleLiClick = (index) => {
+    const lis = mobileNavbarRef.current.querySelectorAll('.MobileNavbarLi');
+    lis.forEach((li, i) => {
+      if (i === 0) {
+        return;
+      }
+      if (i === index) {
+        li.style.color = '#E6007A';
+      } else {
+        li.style.color = '';
+      }
+    });
+  };
+
   return (
     <>
       {isVisible && <div className={overlayClassName} onClick={hideNavbar} ref={overlayRef}></div>}
       {isVisible && (
         <div className={mobileNavbarClassName} ref={mobileNavbarRef}>
-          {/* Mobile navbar content */}
+          <ul className="MobileNavbarUl">
+            <li className="MobileNavbarLi" style={{ color: '#E6007A' }} onClick={() => {handleLiClick(0); console.log('home');}}><HouseIcon className="house-icon" />Home</li>
+            <li className="MobileNavbarLi" onClick={() => {handleLiClick(1); console.log('myaccount');}}><PersonIcon className="person-icon" />My Account</li>
+            <li className="MobileNavbarLi" onClick={() => {handleLiClick(2); console.log('getstarted');}}><PlayArrowIcon className="play-arrow-icon" />Get Started</li>
+            <li className="MobileNavbarLi" onClick={() => {handleLiClick(3); console.log('defi');}}><AccountBalanceWalletIcon className="account-wallet-icon" />DeFi</li>
+          </ul>
         </div>
       )}
     </>
